0f237f11d4cf47577d7b42dcd34d3de89982241e
nguyentu9 <huynhthanhtoan517@gmail.com>
2021-06-07 23:20:21 +0700

    Các dạng phương thức gửi socket cho client

--- index.js
+++ index.js
@@ -9,15 +9,28 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 server.listen(3000)
-
+var sk = []
 io.on('connection', function (socket) {
-    console.log('Co nguoi ket noi: ', socket.id)
-    socket.on('demo', function (data) {
-        console.log(data)
-    })
+    console.log('Co nguoi ket noi: ' + socket.id)
+    sk.push(socket.id)
+    console.log(sk)
+    socket.on('Client_send_data', function (data) {
+        console.log(socket.id + ' da gui: ' + data)
 
+        // Gửi cho tất cả client
+        io.sockets.emit('Server_send_data', data + '888')
+
+        // Gửi lại cho client vừa gửi lên
+        socket.emit('Server_send_data', data + '888')
+
+        // Gửi cho tất cả client ngoại trừ client vừa gửi lên
+        socket.broadcast.emit('Server_send_data', data + '888')
+
+        // Gửi cho client được chỉ định
+        socket.to(`${sk[0]}`).emit('Server_send_data', data + '888')
+    })
     socket.on('disconnect', function () {
-        console.log(socket.id + ' ngat ket noi!!!')
+        console.log(socket.id + ' da ngat ket noi!!!')
     })
 })
 
